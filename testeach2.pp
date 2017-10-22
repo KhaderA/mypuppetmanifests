f565a9484a232f5e504a64718be0c2c30cde379d
KhaderA <ska_mohideen@yahoo.co.in>
2017-10-22 14:27:17 +0530

    adding commit to each test 2 manifest

--- testeach2.pp
+++ testeach2.pp
@@ -4,3 +4,8 @@ $data.each |$items| {
    message => $items[1]
  }
 }
+
+
+## In this each example, the variable data is holding hash(key/value) data. And only one parameter is used to iterate the values of $data.
+## So, the lamba function $item will store every value of $data as an array something like $item[0] stores all the keys and $item[1] stores
+## all the values
